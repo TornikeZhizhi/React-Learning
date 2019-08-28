f01c907469747b2eefff30bb731be31f69aee255
tornikezhizhi <zhizhiashvilitornike@gmail.com>
2019-08-28 18:40:14 +0400

    state basic

--- src/App.js
+++ src/App.js
@@ -1,22 +1,49 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Tokmarila  from './Person/Person.js'
 
 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-       <h1>I`m I React App</h1>
-  
-      <Tokmarila name="tornike"/>
-      <Tokmarila name="shaqro">children 2</Tokmarila>
-      <Tokmarila name="giusha" />
-      </header>
-    </div>
-  );
+class App extends Component {
 
+  state = {
+    persons: [
+
+      {name:"toka", age:"22"},
+      {name:"toka2", age:"23"},
+      {name:"toka2", age:"21"}
+
+    ]
+  }
+
+  switchNameHandeler = () => {
+
+    this.setState({
+      persons:[
+
+        {name:"tokaguja", age:"22"},
+        {name:"toka2", age:"23"},
+        {name:"toka2", age:"21"}
+
+    ]
+  })
+
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+        <h1>I`m I React App</h1>
+
+        <button onClick={this.switchNameHandeler}>Switch Naame</button>
+        <Tokmarila name={this.state.persons[0].name}/>
+        <Tokmarila name={this.state.persons[1].name}>children 2გ</Tokmarila>
+        <Tokmarila name={this.state.persons[2].name} />
+        </header>
+      </div>
+    );
+  }
   // return React.createElement('div',{className: 'App'}, React.createElement('h1',null,'I Am React App'))
 }
 
