3714afb93d3256756baaae2b9769797541519ad6
tornikezhizhi <zhizhiashvilitornike@gmail.com>
2019-08-31 16:15:05 +0400

    two way of binding

--- src/App.js
+++ src/App.js
@@ -16,14 +16,14 @@ class App extends Component {
     ]
   }
 
-  switchNameHandeler = () => {
+  switchNameHandeler = (newName) => {
 
     this.setState({
       persons:[
 
-        {name:"tokaguja", age:"22"},
-        {name:"toka2", age:"23"},
-        {name:"toka2", age:"21"}
+        {name:newName, age:"22"},
+        {name:"nino1", age:"23"},
+        {name:"nino1", age:"21"}
 
     ]
   })
@@ -36,8 +36,8 @@ class App extends Component {
         <header className="App-header">
         <h1>I`m I React App</h1>
 
-        <button onClick={this.switchNameHandeler}>Switch Naame</button>
-        <Tokmarila name={this.state.persons[0].name}/>
+        <button onClick={()=> this.switchNameHandeler("maximilian")}>Switch Naame</button>
+        <Tokmarila click={this.switchNameHandeler.bind(this,'kaxa2')} name={this.state.persons[0].name}/>
         <Tokmarila name={this.state.persons[1].name}>children 2გ</Tokmarila>
         <Tokmarila name={this.state.persons[2].name} />
         </header>
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -7,10 +7,12 @@ const person = (props) => {
     return(
 
       <div>
-     <h1>I am {props.name} {Math.floor(Math.random() * 100)} years old</h1>
+     <h1 onClick={props.click}>I am {props.name} {Math.floor(Math.random() * 100)} years old</h1>
         <p>{props.children}</p>
       </div>  
      )
 }
 
 export default person;
+
+ 
