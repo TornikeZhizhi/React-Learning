b27a5a70ce4da7e8485b0c16fb0609b55488b2d5
tornikezhizhi <zhizhiashvilitornike@gmail.com>
2019-09-03 18:52:45 +0400

    s

--- src/Component/Blocks.js
+++ src/Component/Blocks.js
@@ -16,7 +16,10 @@ const images = importAll(require.context('./img', false, /\.(png|jpe?g|svg)$/));
     { id: 2, src: 'about-artist.png', title: 'Main Art', description: 'Painting Of Art' },
     { id: 3, src: 'art.jpg', title: 'Grand', description: 'Painting Of Art' },
     { id: 4, src: 'child.png', title: 'Foo', description: 'Painting' },
-    { id: 5, src: 'art.jpg', title: 'None', description: 'Painting Of Art' }
+    { id: 5, src: 'art.jpg', title: 'None', description: 'Painting Of Art' },
+    { id: 6, src: 'art.jpg', title: 'None', description: 'Painting Of Art' },
+    { id: 7, src: 'child.png', title: 'None', description: 'Painting Of Art' },
+    { id: 8, src: 'art.jpg', title: 'None', description: 'Painting Of Art' }
   ];
 
 const block = (props) => {
