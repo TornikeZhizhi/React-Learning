5250f844f2f2b323e3ecaf122196bb22bf3c237e
Tornike Zhizhiashvili <t.zhizhiashvili@adjarabet.com>
2022-01-14 02:17:43 +0400

    new condition

--- src/Component/Expense/ExpensesList.js
+++ src/Component/Expense/ExpensesList.js
@@ -4,28 +4,40 @@ import ExpenseItem from "./ExspenseItem";
 const ExpenseList = props => {
 
 
-let expenseContent = <p>no expense found</p>
+// let expenseContent = <p>no expense found</p>
 
-if (props.items.length>0) {
+// if (props.items.length>0) {
     
-    expenseContent = props.items.map((expense) => (
-        <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount} 
-            date={expense.date}>
-        </ExpenseItem>
-     ))
+//         expenseContent = props.items.map((expense) => (
+//             <ExpenseItem
+//                 key={expense.id}
+//                 title={expense.title}
+//                 amount={expense.amount} 
+//                 date={expense.date}>
+//             </ExpenseItem>
+//         ))
+// }
+
+if (props.items.length ===0) {
+    return <h2>Found no expenses</h2>
 }
 
 
 
-
 return (
-    <div>
-
-        {expenseContent}
-    </div>
+    <ul>
+        {
+        props.items.map((expense) => (
+            <ExpenseItem
+                key={expense.id}
+                title={expense.title}
+                amount={expense.amount} 
+                date={expense.date}>
+            </ExpenseItem>
+        ))
+        }
+       
+    </ul>
 )
 
 
